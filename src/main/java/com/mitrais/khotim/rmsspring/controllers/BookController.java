1ac291da05b933e2da7e29cb304020839980d16e
akmitrais <abdulaziz.khotim@mitrais.com>
2018-11-26 10:13:23 +0700

    Add update operation on client.

--- src/main/java/com/mitrais/khotim/rmsspring/controllers/BookController.java
+++ src/main/java/com/mitrais/khotim/rmsspring/controllers/BookController.java
@@ -1,14 +1,6 @@
 package com.mitrais.khotim.rmsspring.controllers;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import javax.servlet.http.HttpServletRequest;
-
+import com.mitrais.khotim.rmsspring.models.Book;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.Resource;
@@ -17,15 +9,15 @@ import org.springframework.hateoas.client.Traverson;
 import org.springframework.hateoas.mvc.TypeReferences.ResourcesType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import com.mitrais.khotim.rmsspring.models.Book;
+import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 @Controller(value = "bookController-client")
 @RequestMapping("/books")
@@ -42,9 +34,9 @@ public class BookController {
 	/**
 	 * Show all books.
 	 *
-	 * @param model
-	 * @return
-	 * @throws URISyntaxException
+	 * @param model Holder for model attributes.
+	 * @return View template.
+	 * @throws URISyntaxException Exception when string couldn't be passed as URI reference.
 	 */
 	@GetMapping
 	public String index(Model model) throws URISyntaxException {
@@ -54,7 +46,7 @@ public class BookController {
 			.toObject(new ResourcesType<Resource<Book>>(){});
 
 		List<Book> books = resources.getContent().stream()
-				.map(r -> r.getContent()).collect(Collectors.toList());
+				.map(Resource::getContent).collect(Collectors.toList());
 		
 		model.addAttribute("books", books);
 		model.addAttribute("book", new Book());
@@ -65,9 +57,9 @@ public class BookController {
 	/**
 	 * Creates a new book.
 	 *
-	 * @param book
-	 * @return
-	 * @throws URISyntaxException
+	 * @param book The book data.
+	 * @return View template.
+	 * @throws URISyntaxException Exception when string couldn't be passed as URI reference.
 	 */
 	@PostMapping
 	public String create(@ModelAttribute Book book) throws URISyntaxException {
@@ -84,9 +76,9 @@ public class BookController {
 	/**
 	 * Updates an existing book.
 	 *
-	 * @param book
-	 * @return
-	 * @throws URISyntaxException
+	 * @param newBook The book data.
+	 * @return View template.
+	 * @throws URISyntaxException Exception when string couldn't be passed as URI reference.
 	 */
 	@RequestMapping(value = "/{id}/update", method = {RequestMethod.GET, RequestMethod.POST})
 	public String update(@ModelAttribute Book newBook,
@@ -95,10 +87,13 @@ public class BookController {
 			HttpServletRequest request) throws URISyntaxException
 	{
 		Traverson client = new Traverson(new URI(REMOTE_SERVICE_ROOT_URI), MediaTypes.HAL_JSON);
-		if (request.getMethod() == RequestMethod.POST.toString()) {
-			Link booksLink = client.follow("books").asLink();
 
-			this.rest.put(booksLink.expand().getHref(), newBook, Book.class);
+		if (request.getMethod().equals(RequestMethod.POST.toString())) {
+			List<String> booksLink = client
+					.follow("books")
+					.toObject("$._embedded.bookList[?(@.id==" + id + ")]._links.self.href");
+
+			this.rest.put(booksLink.get(0), newBook, Book.class);
 
 			return "redirect:/books";
 		}
@@ -106,20 +101,22 @@ public class BookController {
 		Resources<Resource<Book>> resources = client
 				.follow("books")
 				.toObject(new ResourcesType<Resource<Book>>(){});
+
 		List<Book> books = resources.getContent().stream()
-				.map(r -> r.getContent()).collect(Collectors.toList());
-		
-		Map<String, Object> params = new HashMap<>();
-	    params.put("id", id);
-	    
-	    Book resource = client
-				.follow("books")
-				.follow("$._embedded.bookList[0]._links.self.href")
-				.toObject(Book.class);
+				.map(Resource::getContent)
+				.collect(Collectors.toList());
+
+		Book book = new Book();
+		for (Book b: books) {
+			if (b.getId().equals(id)) {
+				book = b;
+				break;
+			}
+		}
 
 		model.addAttribute("books", books);
-		model.addAttribute("book", resource);
-		System.out.println(resource);
+		model.addAttribute("book", book);
+		System.out.println(book);
 		
 		return "book/index";
 	}
