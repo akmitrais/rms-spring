b00d834b556952fe7d8c430ac865ed0ce54d2240
akmitrais <abdulaziz.khotim@mitrais.com>
2018-11-27 17:38:25 +0700

    Set mvc security to ignore '/api/**' path.

--- src/main/java/com/mitrais/khotim/rmsspring/configs/WebSecurityConfig.java
+++ src/main/java/com/mitrais/khotim/rmsspring/configs/WebSecurityConfig.java
@@ -2,8 +2,10 @@ package com.mitrais.khotim.rmsspring.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,11 +23,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")
                 .permitAll()
                 .and()
                 .logout()
                 .permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().mvcMatchers("/api/**");
     }
 
     @Override
